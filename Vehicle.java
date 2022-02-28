2870e4127c305257eab32a55d1ba9d3a827188e1
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2022-02-28 15:02:44 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -4,12 +4,22 @@ class Vehicle {
     System.out.println("poamap poamap");
   }
 }
+class Truck extends Vehicle {
+  public String modelName = "TATA"; 
+    public void honk1() {
+    System.out.println("Horn please poamap poopopop ");
+  }
+}
 
-class Car extends Vehicle {
-  private String modelName = "Mustang";
+class Car extends Truck {
+  private String modelName1 = "Maruti";
   public static void main(String[] args) {
     Car myFastCar = new Car();
-    myFastCar.honk();
-    System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+   myFastCar.honk();
+    myFastCar.honk1();
+    System.out.println("Model name" + myFastCar.brand);
+    System.out.println("Model name" + myFastCar.modelName);
+     System.out.println("Model name" + myFastCar.modelName1);
   }
+
 }
